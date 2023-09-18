8fc902fb671649d3b9ec75b5f4e887cdc8664476
jonathan <jorgejonatthan@gmail.com>
2023-09-17 19:26:42 -0600

    first commit

---
+++ src/client/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="home">
+            <h1>App con Vite + Express</h1>
+        </div>
+    );
+}
+
+export default App;
